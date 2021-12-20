6c5f46c0ffecb3e55fd51c53ec0aef930331933d
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2021-12-20 17:03:24 +0300

    Done exercise 1.4

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,61 +1,56 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
+ 
+  // Exercice 1.3
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
+  const parts=[
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+   {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    ,{
+        name: 'State of a component',
+        exercises: 14
+      }
+  ]
+  console.log(parts[0].name);
   return (
-    <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}  />
-      <br/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    <div className='app'>
+      <Header course={course} />
+      <Contents topic={parts[0].name} />
+      <Contents topic={parts[1].name} />
+      <Contents topic={parts[2].name} />
+      <Total total1={parts[0].exercises} total2={parts[1].exercises} total3={parts[2].exercises} />
     </div>
   )
 }
 
 const Header =({course})=>{
   return(
-    <>
-      <div>
-        <h1>{course}</h1>
-      </div>
-    </>
-  )
-}
-
-const Content =({part1, part2, part3, exercises1, exercises2, exercises3})=>{
-  return(
-    <>
-      <div>
-        <Part part={part1} exercises={exercises1}/>
-        <Part part={part2} exercises={exercises2}/>
-        <Part part={part3} exercises={exercises3}/>
-      </div>
-    </>
-  )
-}
-
-const Part =({part, exercises})=>{
-  return(
     <div>
-      <p>{part} exercises are {exercises}</p>
+      <h1>{course}</h1>
     </div>
   )
 }
-
-const Total=({exercises1, exercises2, exercises3})=>{
+const Contents =({ topic})=>{
   return(
-    <>
-     <div>
-        <p>Total exercises are {exercises1+ exercises2 + exercises3}</p>
-     </div>
-    </>
+    <div>
+      <p>{topic} </p>
+    </div>
+  )
+}
+const Total =({total1, total2, total3})=>{
+  return(
+    <div>
+      <h4>The total exeecises are</h4>
+      <p>{total1 + total2 +  total3}</p>
+    </div>
   )
 }
 
